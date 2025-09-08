061244b6d06f2f492c08c239791efc71065f3b0d
Frédéric Barthelet <frederic.barthelet@gmail.com>
2025-09-08 09:42:48 +0200

    Add first test runner

---
+++ src/commands/run.ts
@@ -0,0 +1,161 @@
+import { Client } from "@modelcontextprotocol/sdk/client";
+import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
+import { ListToolsResult } from "@modelcontextprotocol/sdk/types.js";
+import { Args, Command, Flags } from "@oclif/core";
+import { readFile } from "node:fs/promises";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { OpenAI } from "openai";
+import { ChatCompletionCreateParams } from "openai/resources";
+import { parse } from "yaml";
+import z from "zod";
+
+const TestCaseSchema = z
+  .string()
+  .transform((rawYmlContent) => parse(rawYmlContent))
+  .pipe(
+    z.object({
+      test_cases: z.array(
+        z.object({
+          name: z.string(),
+          input_prompt: z.string(),
+          expected_tool_call: z.object({
+            tool_name: z.string(),
+          }),
+        })
+      ),
+    })
+  );
+
+export default class Run extends Command {
+  static override args = {
+    tests: Args.file({
+      description: "YML file containing the test suite",
+      parse: (file) => readFile(file, "utf8"),
+      required: true,
+    }),
+  };
+  static override description = "Run the test suite";
+  static override examples = ["<%= config.bin %> <%= command.id %>"];
+  static override flags = {
+    assistant: Flags.string({
+      char: "a",
+      default: "anthropic/claude",
+      description:
+        "Assistant configuration to use (impact model and system prompt)",
+      options: ["anthropic/claude"],
+    }),
+    url: Flags.url({
+      char: "u",
+      description: "URL of the MCP server",
+      required: true,
+    }),
+    openRouterApiKey: Flags.string({
+      char: "k",
+      description: "OpenRouter API key to use",
+      required: true,
+      env: "OPENROUTER_API_KEY",
+    }),
+  };
+
+  public async run(): Promise<void> {
+    const {
+      args: { tests },
+      flags: { assistant, url, openRouterApiKey },
+    } = await this.parse(Run);
+
+    const { test_cases: testCases } = TestCaseSchema.parse(tests);
+
+    this.log("🔍 Connecting to the MCP server...");
+    const client = new Client({ name: "mcp-eval", version: "0.0.1" });
+    const transport = new StreamableHTTPClientTransport(url);
+    await client.connect(transport);
+
+    const { tools } = await client.listTools();
+    this.log(
+      `✅ Connected with ${client.getServerVersion()?.name}. ${
+        tools.length
+      } tools found.`
+    );
+
+    const test = testCases[0];
+    await this.runTest({
+      inputPrompt: test.input_prompt,
+      expectedToolCall: { toolName: test.expected_tool_call.tool_name },
+      tools,
+      openRouterApiKey,
+    });
+
+    await transport.terminateSession();
+    await client.close();
+    this.exit(0);
+  }
+
+  static formatToolToMessage(
+    tool: ListToolsResult["tools"][number]
+  ): Exclude<ChatCompletionCreateParams["tools"], undefined>[number] {
+    return {
+      type: "function",
+      function: {
+        name: tool.name,
+        description: tool.description,
+        parameters: {
+          type: "object",
+          properties: tool.inputSchema["properties"],
+          required: tool.inputSchema["required"],
+        },
+      },
+    };
+  }
+
+  private async runTest({
+    inputPrompt,
+    expectedToolCall,
+    tools,
+    openRouterApiKey,
+  }: {
+    inputPrompt: string;
+    expectedToolCall: {
+      toolName: string;
+    };
+    tools: ListToolsResult["tools"];
+    openRouterApiKey: string;
+  }) {
+    const openai = new OpenAI({
+      baseURL: "https://openrouter.ai/api/v1",
+      apiKey: openRouterApiKey,
+    });
+
+    const response = await openai.chat.completions.create({
+      model: "anthropic/claude-3.7-sonnet",
+      tools: tools.map(Run.formatToolToMessage),
+      messages: [
+        {
+          role: "system",
+          content: await readFile(
+            resolve(
+              dirname(fileURLToPath(import.meta.url)),
+              "../prompts",
+              "claude-3.7.md"
+            ),
+            "utf8"
+          ),
+        },
+        { role: "user", content: inputPrompt },
+      ],
+    });
+
+    const toolCall = response.choices[0].message.tool_calls?.[0];
+    if (!toolCall || toolCall.type !== "function") {
+      this.error("No tool were called");
+    }
+
+    if (toolCall.function.name !== expectedToolCall.toolName) {
+      this.error(
+        `Expected tool call ${expectedToolCall.toolName} but got ${toolCall.function.name}`
+      );
+    }
+
+    this.log("✅ Tool call was successful");
+  }
+}
